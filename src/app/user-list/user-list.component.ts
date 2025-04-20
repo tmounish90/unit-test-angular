c2e31b93e2dd6614ec7d886720c01e0630611c3b
tmounish90 <thakkalamounish@gmail.com>
2025-04-19 23:19:31 -0400

    Update user-list.component.ts

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,6 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 import { UsersService } from './../users.service';
 import { User } from './../user';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent {
+  users = [
+    { name: 'Alice' },
+    { name: 'Bob' },
+    { name: 'Charlie' }
+  ];
+}
 
 @Component({
   selector: 'app-user-list',
